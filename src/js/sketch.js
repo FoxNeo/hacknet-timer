c9b69e8d86d78f9e8d1b9d7dee5f8ceaecdac465
foxneo <mcaceres@uni-bremen.de>
2018-10-29 16:55:59 +0100

    Force desktop view to mobile devices

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,3 +1,10 @@
+let viewMode = getCookie("view-mode");
+if(viewMode == "desktop"){
+    viewport.setAttribute('content', 'width=1024');
+}else if (viewMode == "mobile"){
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+}
+
 let counter = 0;
 let timeleft = 35;
 let milis = 100;
